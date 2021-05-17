d4f9237465969d46bc2f8998b10563449bfc9a6a
maciej.koziara <maciej.koziara@airhelp.com>
2021-05-17 20:42:14 +0200

    4th exercise solution

--- src/main/java/pl/infoshare/springdi/airports/provider/file/FileAirportProvider.java
+++ src/main/java/pl/infoshare/springdi/airports/provider/file/FileAirportProvider.java
@@ -1,6 +1,7 @@
 package pl.infoshare.springdi.airports.provider.file;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.infoshare.springdi.airports.model.Airport;
 import pl.infoshare.springdi.airports.model.Continent;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 @Component
 @FileSource
+@Profile("file")
 @RequiredArgsConstructor
 class FileAirportProvider implements AirportProvider {
 
